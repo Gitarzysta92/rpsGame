0b1f16b2f0bacd9d935c1eede6225d02ab307ca9
Michał Łukasiewicz <michal@threesixty.com>
2018-10-01 19:40:45 +0200

    Some fix for game logic

--- js/script.js
+++ js/script.js
@@ -6,9 +6,6 @@ var figures = ['scissors', 'paper', 'rock'];
 document.body.addEventListener('click', function(event){
 	var element = event.target,
 		figure = figures.indexOf(event.target.dataset.figure);
-
-		
-
 		getWinner(figure);
 })
 
@@ -22,10 +19,12 @@ function getWinner(playerFigure) {
 	var player = playerFigure,
 		oponent = oponentFigure();
 
-	
-
-	if (player < oponent){
-		console.log(playerFigure, oponentFigure());	
+	if ( player === oponent - 1 || player === 2 && oponent === 0 ){
+		console.log('Winner: ', figures[player], figures[oponent]);	
+	} else if ( player === oponent) {
+		console.log('it\' draw', player, oponent); 
+	} else {
+		console.log('You have lost', player, oponent);
 	}
 }
 
