705f1fc938640b079625ba657fb7d8089d95bdeb
Michał Łukasiewicz <michal@threesixty.com>
2019-07-03 00:24:55 +0200

    Add form validation

--- src/js/modules/new-game-form/newGameForm.js
+++ src/js/modules/new-game-form/newGameForm.js
@@ -3,6 +3,12 @@ import { core, utils } from '../../app.js';
 const { controller, view } = core;
 const { animation } = utils;
 
+// error messages
+const errorNoName = 'You are not specified your name';
+const errorNoRounds = 'You are not specified rounds number';
+const errorWrongRounds = 'Rounds field must be a number';
+
+
 // initialize default form
 const gameSetupForm = view.element({
 	selector: '#form',
@@ -17,18 +23,60 @@ const gameSetupForm = view.element({
 			data.preventDefault();
 
 			const inputs = element.domInstance.querySelectorAll('input'); 
-			const results = {};
+			const results = {}
 
 			inputs.forEach(input => Object.defineProperty(results, input.id, {
 				value: input.value,
 				writable: false	
 			}));
-			controller.invokeAction('submitted-data', results);
+
+			const isValid = validateForm(results);
+			isValid && controller.invokeAction('submitted-data', results);
 		}
 	}
 });
 
 controller.defineAction('form-submit', { exec: gameSetupForm.onSubmit, async: false });
-
-
 export default gameSetupForm;
+
+// form validate
+function validateForm({name, rounds}) {
+	const errors = [];
+	
+	if (name.length === 0) errors.push(errorNoName);
+	if (rounds.length === 0) errors.push(errorNoRounds);
+	if (rounds.length > 0 && !Number(rounds)) errors.push(errorWrongRounds);
+
+	errors.length > 0 && controller.invokeAction('invalid-form-data_alert', errors);
+	return errors.length > 0 ? false : true;
+}
+
+
+
+
+// initialize alert box
+const alertBox = view.element({
+	selector: '#alert-box',
+	customProperties: { 
+		show: function(errors) {
+			this.domInstance.innerHTML = '';
+			this.domInstance.style.display = 'block';
+			const div = document.createElement('div');
+			
+			errors.forEach(error => {
+				const p = document.createElement('p');
+				const text = document.createTextNode(error);
+				p.appendChild(text);
+				div.appendChild(p);
+			})
+			this.domInstance.appendChild(div);
+		},
+		hide: function() {
+			this.domInstance.style.display = 'none';
+			this.domInstance.innerHTML = '';
+		}
+	}
+})
+
+controller.defineAction('submitted-data', { exec: alertBox.hide, async: false })
+controller.defineAction('invalid-form-data_alert', { exec: alertBox.show, async: false })
